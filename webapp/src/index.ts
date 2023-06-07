5e739a656370a402190fafb60840ae0b818ea4f9
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2023-06-07 17:18:04 +0200

    add basic configuration for openapi

--- webapp/src/index.ts
+++ webapp/src/index.ts
@@ -1,23 +1,90 @@
-import Fastify from 'fastify'
+import Fastify from "fastify";
+import FastifySwagger from "@fastify/swagger";
 
-const port : number = Number(process.env.PORT) || 5000;
+const port: number = Number(process.env.PORT) || 5000;
 let numVisits = 0;
 
-const fastify = Fastify({
-  logger: true
-});
+Promise.resolve().then(main).catch(console.error);
 
-// Declare a route
-fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world', visits : numVisits++  })
-});
+async function main() {
+  const fastify = Fastify({
+    logger: true,
+  });
 
-// Run the server!
-fastify.listen({ port, host: '0.0.0.0' }, function (err, address) {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  } else {
-    fastify.log.info(`Web application is listening on ${address}`)
-  }
-});
+  await fastify.register(FastifySwagger, {
+    swagger: {
+      info: {
+        title: "Test swagger",
+        description: "Testing the Fastify swagger API",
+        version: "0.1.0",
+      },
+      externalDocs: {
+        url: "https://swagger.io",
+        description: "Find more info here",
+      },
+      host: "localhost",
+      schemes: ["http"],
+      consumes: ["application/json"],
+      produces: ["application/json"],
+      tags: [
+        { name: "user", description: "User related end-points" },
+        { name: "code", description: "Code related end-points" },
+      ],
+      definitions: {
+        User: {
+          type: "object",
+          required: ["id", "email"],
+          properties: {
+            id: { type: "string", format: "uuid" },
+            firstName: { type: "string" },
+            lastName: { type: "string" },
+            email: { type: "string", format: "email" },
+          },
+        },
+      },
+      securityDefinitions: {
+        apiKey: {
+          type: "apiKey",
+          name: "apiKey",
+          in: "header",
+        },
+      },
+    },
+  });
+
+  fastify.get('/', {
+    schema: {
+      operationId: 'root-endpoint',
+      response: {
+        200: {
+          description: 'Expected Response',
+          type: 'object',
+          properties: {
+            hello: {type: 'string' },
+            visits: { type : 'number'}
+          }
+        }
+      }
+    },
+    config: {
+      swagger: {
+        exposeHeadRoute: true,
+      }
+    }
+  }, (req, reply) => {
+    reply.send({ hello: 'world', visits : numVisits++  })
+  })
+
+  await fastify.ready();
+  fastify.swagger();
+
+  // Run the server!
+  fastify.listen({ port, host: '0.0.0.0' }, function (err, address) {
+    if (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    } else {
+      fastify.log.info(`Web application is listening on ${address}`)
+    }
+  });
+}
