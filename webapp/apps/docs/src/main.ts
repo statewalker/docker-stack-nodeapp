9cb214262825d2e34f7846929f7e82ad01b35f74
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2024-01-30 09:06:22 +0100

    chore: update project configurations

--- webapp/apps/docs/src/main.ts
+++ webapp/apps/docs/src/main.ts
@@ -3,7 +3,6 @@ import "./style.css";
 import typescriptLogo from "./typescript.svg";
 import { Counter } from "@repo/ui/counter";
 import { setupCounter } from "@repo/ui/setup-counter";
-import { hello } from "@repo/core";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
@@ -15,7 +14,6 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     </a>
     ${Header({ title: "Docs" })}
     <div class="card">
-      ${hello()}: 
       ${Counter()}
     </div>
   </div>
